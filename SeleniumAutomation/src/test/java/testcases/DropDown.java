3e87bce91f4d983873d6cf7be2711696c10aa050
aamir290895 <aamir.007sdl.ak@gmail.com>
2021-11-22 01:54:59 +0530

    updated

---
+++ SeleniumAutomation/src/test/java/testcases/DropDown.java
@@ -0,0 +1,16 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropDown {
+	public static void handleDropDown(WebDriver driver) throws InterruptedException {
+		driver.get("https://www.calculator.net/interest-calculator.html");
+		Thread.sleep(1000);
+		Select dropdown = new Select(driver.findElement(By.xpath("//*[@id=\"ccompound\"]")));
+		dropdown.selectByVisibleText("continuously");
+		Thread.sleep(2000);
+		driver.close();
+	}
+}
